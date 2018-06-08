4c7cb961de22fb7ae4bcdae005df3ece698b92a2
yizhicheng <253921698@qq.com>
2018-06-08 17:23:46 +0800

    '第一次'

---
+++ compile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys,os,shutil,stat,time
+version = int( time.time() )
+if os.path.exists('./build/'):
+  shutil.rmtree('./build/')
+
+# copy all file to build dir
+shutil.copytree('./','./build/', symlinks=False, ignore=shutil.ignore_patterns('*.py', '*.docx', 'deploy.js','.DS_Store', 'package-lock.json', 'build') )
+list = ['./build/css/style.css', './build/index.html','./build/book.html','./build/member.html','./build/goe.html','./build/plan.html', './build/ico.html' ]
+
+def repip_func(file_path):
+
+  f = open(file_path,'r+',encoding='UTF-8')
+  all_the_lines = f.readlines()
+  f.seek(0)
+  f.truncate()
+  for line in all_the_lines:
+    line = line.replace('{version}', str(version) )
+    f.write(line)
+  f.close()
+
+for file_path in list:
+  repip_func(file_path)
+# deploy to server
+os.system('rsync --delete -avrg ./build/* root@47.75.103.224:/var/www/goe/pc');
